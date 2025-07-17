b372cf2169641c1c115a82a89a4f9e9d93a22c93
NatiGirma <natigirma.code@gmail.com>
2025-07-17 16:18:48 +0300

    basic operation completed

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,6 @@
+number1 = 10
+number2 = 5
+
+print("sum of " + str(number1) + " and " + str(number2) + " is " + str(number1 + number2) )
+print("difference of " + str(number1) + " and " + str(number2) + " is " + str(number1 - number2) )
+print("product of " + str(number1) + " and " + str(number2) + " is " + str(number1 * number2) )
