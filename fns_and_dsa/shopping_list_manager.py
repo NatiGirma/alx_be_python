f2137a7c34d855a67c4e084aff0f9f1b112d3304
NatiGirma <natigirma.code@gmail.com>
2025-07-26 14:11:39 +0300

    shopping manager input number fixed

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,25 +12,33 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("Enter item to add: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
-            print(f"{item} added to list.")
+            print(f"{item} added to the list.")
+        
         elif choice == '2':
-            item = input("Enter item to remove: ")
+            item = input("Enter the item to remove: ")
             if item in shopping_list:
                 shopping_list.remove(item)
-                print(f"{item} removed from list.")
+                print(f"{item} removed from the list.")
             else:
-                print(f"{item} not found in list.")
+                print(f"{item} not found in the list.")
+        
         elif choice == '3':
             print("Current Shopping List:")
-            for i in shopping_list:
-                print(i)
+            if shopping_list:
+                for i in shopping_list:
+                    print(i)
+            else:
+                print("Your shopping list is empty.")
+        
         elif choice == '4':
             print("Goodbye!")
             break
+        
         else:
             print("Invalid choice. Please try again.")
 
 if __name__ == "__main__":
     main()
+    
